dfe0c1870fe9358460b7ce34172c31b461fbc17e
Luis <talkme.lusi@gmail.com>
2024-05-06 15:46:13 -0300

    reorganizando

--- Estudo-python-completo/t.py
+++
@@ -1,7 +0,0 @@
-def minha_funcao(**kwargs):
-    print("Função chamada com os seguintes argumentos:")
-    print(kwargs)  # Verifique se os argumentos estão corretos
-    for chave, valor in kwargs.items():
-        print(f"{chave} : {valor}")
-
-minha_funcao(nome="Alice", idade=30, cidade="São Paulo")
